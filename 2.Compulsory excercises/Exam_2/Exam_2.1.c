e1cd5356fbe77e4ce1ccfc6d509aae139ae67219
NPLinh <ling.npl.1612@gmail.com>
2022-08-04 17:30:34 +0700

    Add file

---
+++ 2.Compulsory excercises/Exam_2/Exam_2.1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int isContainLower(char pswd[])
+{
+    int n = strlen(pswd);
+    for (int i = 0; i < n; i++)
+    {
+        if (islower(pswd[i]))
+            return 1;
+    }
+    return 0;
+}
+
+int isContainUpper(char pswd[])
+{
+    int n = strlen(pswd);
+    for (int i = 0; i < n; i++)
+    {
+        if (isupper(pswd[i]))
+            return 1;
+    }
+    return 0;
+}
+
+int isContainNonletter(char pswd[])
+{
+    int n = strlen(pswd);
+    for (int i = 0; i < n; i++)
+    {
+        if (isalpha(pswd[i]) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+int isValidPassword(char pswd[], FILE *file)
+{
+    if ((isContainLower(pswd)) && (isContainNonletter(pswd)) && (isContainUpper(pswd)))
+        return 1;
+    else
+    {
+        if (isContainLower(pswd) == 0)
+            //printf("Not contain lowercase letter - ");
+            fputs("Not contain lowercase letter - ",file);
+        if (isContainUpper(pswd) == 0)
+            //printf("Not contain uppercase letter - ");
+            fputs("Not contain uppercase letter - ",file);
+        if (isContainNonletter(pswd) == 0)
+            //printf("All is letter character - ");
+            fputs("All is letter character - ",file);
+    }
+    return 0;
+}
+
+int main()
+{
+    FILE *pswfile;
+
+
+
+    FILE *result;
+        result = fopen("result.txt", "w+");
+
+
+
+
+    char line[100];
+    pswfile = fopen("pswfile.txt", "r");
+    if (pswfile == NULL)
+        return 1;
+    int i = 0;
+    while (fgets(line, 100, pswfile))
+    {
+        line[strlen(line) - 1] = '\0';
+        if (isValidPassword(line,result))
+        {
+            fputs("Valid!\n", result);
+            //printf("Valid!\n");
+        }
+        else
+        fputs("Unvalid!\n", result);
+            //printf("Unvalid\n");
+    }
+    fclose(result);
+    fclose(pswfile);
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repo consists of exercises from **Programming Language** Course 20212:
 
-1. Optional exercises (From *Programming in C*)
-2. Compulsory exercises
+1. Optional excercises (From *Programming in C*)
+2. Compulsory excercises
 3. STL Project
 4. Advanced Library (We will introduce **Eigen**)
