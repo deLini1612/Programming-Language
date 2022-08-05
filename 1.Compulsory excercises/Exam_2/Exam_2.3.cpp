bf71ac644e0b37610da4a9826ecd6f7e4f4733f6
NPLinh <ling.npl.1612@gmail.com>
2022-08-05 17:27:17 +0700

    Update file

---
+++ 1.Compulsory excercises/Exam_2/Exam_2.3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+using namespace std;
+int main()
+{
+    map<string, int> hieudz;
+    map<string, int>::iterator it;
+    string str;
+    cout<<" -----------LET'S START -------------"<<endl;
+    while (getline(std::cin, str))
+    {
+
+        if ((it = hieudz.find(str)) != hieudz.end())
+            hieudz[str]++;
+        else
+            hieudz.insert({str, 1});
+    }
+    cout<<"------------THE RESULT_______________"<<endl;
+    for (it = hieudz.begin(); it != hieudz.end(); ++it)
+    {
+        cout << it->first << "   " << it->second
+             << '\n';
+    }
+    return 0;
+}
